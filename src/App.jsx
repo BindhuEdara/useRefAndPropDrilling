6c89014f601730a31c09b672bf000db289c7e08b
BindhuEdara <mandadi.bindu@gmail.com>
2025-12-19 15:34:14 +0530

    worked on useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Component1 from './Component1.jsx';
+import VideoPlayer from './VideoPlayer.jsx';
 
 function App() {
 
   return (
     <>
+      <VideoPlayer />
       <Component1 />
     </>
   )
